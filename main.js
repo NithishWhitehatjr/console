3c55b0a8a5fcee78322438513840d3ca21a5f171
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-03-27 11:46:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var title = "My first console project";
+console.log(title);
+var first_name = "Nithish";
+var last_name = "prakash";
+var full_name =first_name.concat(last.name);
+console.log(full_name);
+var number1 = "12";
+console.log(number1);
+var number2 = "23";
+console.log(number2);
+var plus = "35";
+console.log(plus);
+var subtractedby = "11";
+console.log(subtractedby);
+var multipliedby = "276";
+console.log(multipliedby);
+var dividedby = "1.91666667";
+console.log(dividedby);
+
+
+
+
