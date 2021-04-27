9347df7dbc049c8a1b4ab581a21b0bd0d1e65e03
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-04-27 11:54:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
 var title = "My first console project";
 console.log(title);
 var first_name = "Nithish";
 var last_name = "prakash";
-var full_name =first_name.concat(last.name);
+var full_name = first_name.concat(last.name);
 console.log(full_name);
 var number1 = "12";
 console.log(number1);
@@ -18,6 +17,5 @@ console.log(multipliedby);
 var dividedby = "1.91666667";
 console.log(dividedby);
 
-
-
-
+<
+script > //printing a message var message = "Hello! I am your console"; console.log(message); //Printing my name by concatenation var firstname = "Nithish "; var lastname = "Prakash"; var concat = firstname.concat(lastname); console.log(concat); //Addition of two numners var firstno = 2; var secondno = 4; var addition = firstno + secondno; console.log(addition); //Subtraction of two numbers var firstsnum = 5; var secondsnum = 2; var subtraction = firstsnum - secondsnum; console.log(subtraction); //Multiplication of two numbers var firstmnum = 3; var secondmnum = 4; var multiplication = firstmnum * secondmnum; console.log(multiplication); //Division of two numbers var firstdnum = 4; var seconddnum = 2; var division = firstdnum / seconddnum; console.log(division); </script>
