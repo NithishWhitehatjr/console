3aa1a034883f192cfe61023230633be54298724e
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-06-24 13:20:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,18 +4,15 @@ var first_name = "Nithish";
 var last_name = "prakash";
 var full_name = first_name.concat(last.name);
 console.log(full_name);
-var number1 = "12";
+var number1 = 12;
 console.log(number1);
-var number2 = "23";
+var number2 = 23;
 console.log(number2);
-var plus = "35";
+var plus = number1+number2;
 console.log(plus);
-var subtractedby = "11";
+var subtractedby = number1-number2;
 console.log(subtractedby);
-var multipliedby = "276";
+var multipliedby = number1*number2;
 console.log(multipliedby);
-var dividedby = "1.91666667";
+var dividedby = number1/number2;
 console.log(dividedby);
-
-<
-script > //printing a message var message = "Hello! I am your console"; console.log(message); //Printing my name by concatenation var firstname = "Nithish "; var lastname = "Prakash"; var concat = firstname.concat(lastname); console.log(concat); //Addition of two numners var firstno = 2; var secondno = 4; var addition = firstno + secondno; console.log(addition); //Subtraction of two numbers var firstsnum = 5; var secondsnum = 2; var subtraction = firstsnum - secondsnum; console.log(subtraction); //Multiplication of two numbers var firstmnum = 3; var secondmnum = 4; var multiplication = firstmnum * secondmnum; console.log(multiplication); //Division of two numbers var firstdnum = 4; var seconddnum = 2; var division = firstdnum / seconddnum; console.log(division); </script>
